49483d72aee80f9bee9dd98a3b46220a2b483647
Lars Uffmann <lars.uffmann@vitroconnect.de>
2022-02-26 10:34:09 +0100

    start.spring.io

---
+++ src/main/java/org/cachescrubber/openapitools/demo/springbootjacksonoptionalnullable/SpringBootJacksonOptionalNullableApplication.java
@@ -0,0 +1,13 @@
+package org.cachescrubber.openapitools.demo.springbootjacksonoptionalnullable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJacksonOptionalNullableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJacksonOptionalNullableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/cachescrubber/openapitools/demo/springbootjacksonoptionalnullable/SpringBootJacksonOptionalNullableApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cachescrubber.openapitools.demo.springbootjacksonoptionalnullable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJacksonOptionalNullableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
